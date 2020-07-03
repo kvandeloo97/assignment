035d6cfb9757a4ed1198e2630e98e93dc57dc891
kvandeloo97 <kvand034@uottawa.ca>
2020-07-03 13:38:51 -0400

    Fixed guessinggame.sh based on feedback

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,34 +1,33 @@
-#!/usr/bin/env bash
-# File : guessing.sh
-# Peer-graded assignment (The Unix Workbench)
-
-echo "Welcome to the guessing game!"
-echo "How many files are in the current directory?"
-read guess
-
-function guess_numberfiles {
-	local number_files=$(ls -l | wc -l)
-	echo $number_files
-}
-
-correct_guess=$(guess_numberfiles)
-
-while [[ $guess -ne $correct_guess ]]
-do
-		if [[ $guess > $correct_guess ]]
-		then
-			echo "Your guess is too high. Let's try again."
-
-		elif [[ $guess < $correct_guess ]]
-		then
-			echo "Your guess is too low. Let's try again."
-		fi
-		echo "How many files are in the current directory?"
-		read guess
-done
-
-if [[ $guess == $correct_guess ]]
-then
-	echo "Congratulations! You have guessed correctly."
+if [ -d ]; then
+   	number_files=$(ls -l | wc -l)
 fi
 
+echo "Welcome to the guessing game."
+echo
+echo "Guess the number of files in the current directory."
+read guess
+
+while [[ $guess -ne $number_files ]]
+do
+		if [[ $guess -gt $number_files ]]
+		then
+			echo "Your guess is too high."
+			echo "How many files are in the current directory?"
+			read guess
+
+		elif [[ $guess -lt $number_files ]]
+		then
+			echo "Your guess is too low."
+			echo "How many files are in the current directory?"
+			read guess
+		else
+			echo "Congratulations, you have guessed the correct number of files!"
+			break
+fi
+done
+
+while [[ $guess == $number_files ]]
+do
+	echo "Congratulations, you have guessed the correct number of files!"
+	break
+done
